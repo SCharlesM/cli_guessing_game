566fe406691e562316c68b498e0644d5f84d1961
SCharles <97812129+SCharlesM@users.noreply.github.com>
2025-01-21 21:49:27 +0000

    initialising random number and welcome message

--- number_guessing_game_cli.py
+++ number_guessing_game_cli.py
@@ -3,3 +3,14 @@ Number Guessing Game CLI
 
 From Roadmap.sh Projects:  https://roadmap.sh/projects/number-guessing-game
 """
+import random
+
+random_integer = random.randint(1, 100)
+loop = True
+
+print('Welcome to the number Guessing Game!)
+print("I'm thinking of a number between 1 and 100.")
+print("You have 3, 5 or 10 guesses to find the correct number.")
+
+while(loop):
+
