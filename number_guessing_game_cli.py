eee11c902317ecf6fd8b63181575ae63c107bdd1
SCharles <97812129+SCharlesM@users.noreply.github.com>
2025-01-22 18:04:59 +0000

    refactored intro_loop with dictionary

--- number_guessing_game_cli.py
+++ number_guessing_game_cli.py
@@ -10,38 +10,41 @@ number_of_guesses = 0
 intro_loop = True
 main_loop = True
 
-print('\nWelcome to the number Guessing Game!')
-print("I'm thinking of a number between 1 and 100.")
-print("You have 10, 5 or 3 guesses to find the correct number.")
+print("\nWelcome to the number Guessing Game!"
+    "\nI'm thinking of a number between 1 and 100."
+    "\nYou have 10, 5 or 3 guesses to find the correct number.")
 
-while intro_loop:
-    print('\nWhat difficulty would you like to play?')
-    print('1. Easy 10 guesses\n2. Medium 5 guesses\n3. Hard 3 guesses')
+while intro_loop :
+    print('\nWhat difficulty would you like to play?'
+        '\n1. Easy - 10 guesses. 2. Medium - 5 guesses. 3. Hard - 3 guesses')
+    
+    difficulty = int(input("\nEnter 1, 2 or 3: "))
+    if difficulty < 1 or difficulty > 3 :
+        print("Sorry this is not a valid option")
+        break
 
-    difficulty = int(input('\nEnter 1, 2, or 3: '))
-    if difficulty in ('1', '2', '3') :
-    #if difficulty in range(1, 3) :
-        match difficulty :
-            case '1' :
-                print('You chose difficulty EASY')
-                number_of_guesses = 10
-                break
-            case '2' :
-                print('You chose difficulty MEDIUM')
-                number_of_guesses = 5
-                break
-            case '3' :
-                print('You chose difficulty HARD')
-                number_of_guesses = 3
-                break
-    else:
-        print('\nSorry that is not a valid option')
+    options_dict = {
+        1 : 'EASY',
+        2 : 'MEDIUM',
+        3 : 'HARD',
+    }
+    guesses_dict = {
+        'EASY' : 10,
+        'MEDIUM' : 5,
+        'HARD' : 3,
+    }
+    print("You chose difficulty:", options_dict[difficulty])
+    number_of_guesses = guesses_dict[options_dict[difficulty]]
+    break
+
 intro_loop = False
 
 while (number_of_guesses > 0) :   
     print("\nI'm thinking of a number between 1 and 100")
     guess = int(input('What is your guess? '))
-    if guess == random_integer :
+    if guess < 1 or guess > 100 :
+        print("\nThat is not a valid guess, choose a number between 1 and 100")
+    elif guess == random_integer :
         print('\nCongratulations! You guessed the correct number')
         break
     elif guess > random_integer :
@@ -52,4 +55,20 @@ while (number_of_guesses > 0) :
         print('\nThat guess is too low')
         number_of_guesses -= 1
         print('You have',number_of_guesses,'guesses remaining')
+
+    if number_of_guesses == 0 :
+        print("\nSorry you have run out of guesses")
+        print("The correct number was", random_integer)
+        break   
+
 print("\nGAME OVER")
+
+"""
+if __name__ =="__main__":
+
+    welcome()
+    select_difficulty()
+    guess_loop()
+    end_game()
+
+"""
